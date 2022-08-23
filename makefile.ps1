3908c5c99ab6080c3b2cdbb863c976a67d080b13
Ted Thompson <ted@federalproductions.com>
2022-08-23 01:23:09 -0700

    Update makefile.ps1
    After testing, discovered MS Compress-Archive doesn't make a zip Yagami likes.
    
    Switched to WinRAR.

--- makefile.ps1
+++ makefile.ps1
@@ -9,13 +9,25 @@ Copy-Item src\music\*.* release\music
 Copy-Item src\Gladiator\* release\Gladiator -Recurse
 Copy-Item src\baseq2\players\* release\baseq2\players -Recurse
 Copy-Item src\baseq2\*.txt release\baseq2
-Compress-Archive src\baseq2\env release\baseq2\pak-env.zip
-Compress-Archive src\baseq2\models release\baseq2\pak-models.zip
-Compress-Archive src\baseq2\textures release\baseq2\pak-textures.zip
-Compress-Archive src\baseq2\pics release\baseq2\pak-kmq2_HUD_REpak.zip
-Compress-Archive src\baseq2\kmq2_HUD_REpak_README.txt -update release\baseq2\pak-kmq2_HUD_REpak.zip
-Compress-Archive src\baseq2\mypics\pics release\baseq2\pak-HUD_Supplemental.zip
-Compress-Archive src\baseq2\HUD_Supplemental_README.txt -update release\baseq2\pak-HUD_Supplemental.zip
-cd release\baseq2
+cd src\baseq2
+#
+# Stupid fucking Compress-Archive isn't 100% accurate zip builder it seems...
+# At least not for Yagami to open and read it.
+# Had to flip to using good ol' WinRAR to make a legit ZIP. 
+# Micro$hit strikes again.
+#
+#Compress-Archive src\baseq2\env release\baseq2\pak-env.zip
+winrar a ..\..\release\baseq2\pak-env.zip env | Out-Null
+#Compress-Archive src\baseq2\models release\baseq2\pak-models.zip
+winrar a ..\..\release\baseq2\pak-models.zip models | Out-Null
+#Compress-Archive src\baseq2\textures release\baseq2\pak-textures.zip
+winrar a ..\..\release\baseq2\pak-textures.zip textures | Out-Null
+#Compress-Archive src\baseq2\pics release\baseq2\pak-kmq2_HUD_REpak.zip
+winrar a ..\..\release\baseq2\pak-kmq2_HUD_REpak.zip pics | Out-Null
+#Compress-Archive src\baseq2\mypics\pics release\baseq2\pak-HUD_Supplemental.zip
+cd mypics
+winrar a ..\..\..\release\baseq2\pak-HUD_Supplemental.zip pics | Out-Null
+
+cd ..\..\..\release\baseq2
 Get-ChildItem *.zip | Rename-Item -NewName { $_.Name -replace '.zip','.pk3' }
 Read-Host -prompt "Package assembled and placed in 'release' folder.`n`nPress ENTER to close this window."
